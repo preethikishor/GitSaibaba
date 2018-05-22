19d75aebd31e2ee55e0c21c2a882714c8a6fb040
preethikishor <30550110+preethikishor@users.noreply.github.com>
2018-05-22 22:41:48 +0530

    sdsd

--- GitSaibaba/src/GitS.java
+++ GitSaibaba/src/GitS.java
@@ -2,7 +2,7 @@
 public class GitS {
 
 	public static void main(String[] args) {
-		System.out.println("git moving");
+		System.out.println("gitasd moving");
 		System.out.println("sai moving");
 
 	}
