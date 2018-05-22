bb0571f3e966a03296c039e005f796cc9975e9c0
preethikishor <30550110+preethikishor@users.noreply.github.com>
2018-05-22 23:18:01 +0530

    Update GitS.java

--- GitSaibaba/src/GitS.java
+++ GitSaibaba/src/GitS.java
@@ -2,7 +2,7 @@
 public class GitS {
 
 	public static void main(String[] args) {
-		System.out.println("xxxxx uuuu moving");
+		System.out.println("xxxxx uuuu moving ererer ghghh");
 		System.out.println("%%%% moving");
 		
 		}
